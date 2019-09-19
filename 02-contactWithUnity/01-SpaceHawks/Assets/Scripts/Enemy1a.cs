51fae5bfdbea92a59c416014d5c64c545d8e1e67
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-09-19 18:35:16 +0200

    Background, spaceship, enemy

---
+++ 02-contactWithUnity/01-SpaceHawks/Assets/Scripts/Enemy1a.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1a : MonoBehaviour
+{
+    [SerializeField]private float speedX = 0.8F;
+    [SerializeField] private float speedY = 0.8F;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speedX * Time.deltaTime, speedY * Time.deltaTime, 0);
+        if (transform.position.x > 4 || transform.position.x < -4)
+            speedX = -speedX;
+        if (transform.position.y > 2.5 || transform.position.y < -2.5)
+            speedY = -speedY;
+    }
+}
