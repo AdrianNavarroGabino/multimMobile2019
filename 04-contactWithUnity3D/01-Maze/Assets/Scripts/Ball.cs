82e61e5ec97bc9c175abc03c49e09ccccc0c71b5
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-10-02 17:14:49 +0200

    Enemy

---
+++ 04-contactWithUnity3D/01-Maze/Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody body;
+    float speed = 150f;
+    float rotationSpeed = 150f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float advance = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up, rotation);
+        transform.position += transform.forward * Time.deltaTime * advance;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Tocado");
+        Application.Quit();
+    }
+}
