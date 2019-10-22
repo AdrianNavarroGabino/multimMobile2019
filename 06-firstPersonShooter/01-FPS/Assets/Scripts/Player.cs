b7b89f7ab1c0e8eed6d4b5537092ddc68aa70237
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-10-22 16:01:27 +0200

    Nav mesh agent

---
+++ 06-firstPersonShooter/01-FPS/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool shootHit = false;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Shooting up...");
+            float maxDistance = 100;
+            RaycastHit objectHit;
+            shootHit = Physics.Raycast(camera.transform.position,
+                camera.transform.forward, out objectHit, maxDistance);
+
+            if (shootHit)
+            {
+                Debug.Log("Shocked Shock");
+                if (objectHit.collider.CompareTag("Enemy"))
+                {
+                    Debug.Log("Successful enemy");
+                }
+            }
+        }
+    }
+}
