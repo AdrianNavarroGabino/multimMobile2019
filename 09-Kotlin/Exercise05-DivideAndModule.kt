47151423134424db6738159cc8a2b2bd96c0ab07
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2019-11-20 17:32:08 +0100

    Exercise 06 - Divide and module

---
+++ 09-Kotlin/Exercise05-DivideAndModule.kt
@@ -0,0 +1,14 @@
+// Adrián Navarro Gabino
+
+/*
+ * Divide and module
+ */
+
+fun main() {
+    println("Enter the first number")
+    var a = readLine()!!.toInt()
+    println("Ente the second number")
+    var b = readLine()!!.toInt()
+    println("Their division is: ${a / b}")
+    println("Their module is: ${a % b}")
+}
