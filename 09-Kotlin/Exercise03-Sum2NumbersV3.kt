9973af1d0bea4a0f213be559bf0746f53486deb4
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2019-11-20 17:31:36 +0100

    Example 03 - Sum two numbers

---
+++ 09-Kotlin/Exercise03-Sum2NumbersV3.kt
@@ -0,0 +1,14 @@
+// Adrián Navarro Gabino
+
+/*
+ * Sum 2 numbers (v3)
+ */
+
+fun main() {
+    println("Enter the first number")
+    var a = readLine()!!.toInt()
+    println("Ente the second number")
+    var b = readLine()!!.toInt()
+    var sum = a + b
+    println("Their sum is: $sum")
+}
