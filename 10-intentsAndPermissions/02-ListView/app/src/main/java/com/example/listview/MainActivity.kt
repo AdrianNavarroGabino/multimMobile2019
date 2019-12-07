5f9a5811266a1397bcf4266f0be76da67eebceb6
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2019-12-07 12:47:39 +0100

    Launch activity passing data

---
+++ 10-intentsAndPermissions/02-ListView/app/src/main/java/com/example/listview/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.listview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val subjects = resources.getStringArray(R.array.array_subjects)
+        val adapter = ArrayAdapter(this, R.layout.listview_item, subjects)
+
+        listView.adapter = adapter
+
+        listView.onItemClickListener =
+            object : AdapterView.OnItemClickListener
+            {
+                override
+                fun onItemClick(parent: AdapterView<*>?, view: View?,
+                                position: Int, id: Long) {
+
+                    Snackbar.make(
+                        root_layout,
+                        "Clicked ${listView.getItemAtPosition(position)}",
+                        Snackbar.LENGTH_LONG
+                    ).setAction(
+                        "Accept"
+                    ){
+
+                    }.show()
+                }
+            }
+    }
+}
