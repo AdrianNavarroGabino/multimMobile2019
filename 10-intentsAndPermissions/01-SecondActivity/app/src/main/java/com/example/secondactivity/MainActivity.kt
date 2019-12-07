453c46b30f2af77aad90cf2582ed87345e338dfe
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2019-12-07 12:49:19 +0100

    Launch Activity

---
+++ 10-intentsAndPermissions/01-SecondActivity/app/src/main/java/com/example/secondactivity/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.secondactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        conduccion.setOnClickListener {
+            val myIntent = Intent(this, Conduccion::class.java)
+            startActivity(myIntent)
+        }
+
+        conveccion.setOnClickListener {
+            val myIntent = Intent(this, Conveccion::class.java)
+            startActivity(myIntent)
+        }
+
+        radiacion.setOnClickListener {
+            val myIntent = Intent(this, Radiacion::class.java)
+            startActivity(myIntent)
+        }
+    }
+}
